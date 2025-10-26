1bef2f39110c8d3ff3989594bd528884d193bfb2
MosesAwad <mawad@student.42abudhabi.ae>
2025-10-26 19:04:51 +0400

    Added a __doc__ to the ex06 ft_filter.py functions

--- 0-Starting/code/ex06/ft_filter.py
+++ 0-Starting/code/ex06/ft_filter.py
@@ -1,10 +1,16 @@
 
 def ft_filter(function, iterable):
+    """
+    Filter items in `iterable` using `function` as the predicate.
+    """
     result_arr = [i for i in iterable if function(i) is True]
     return result_arr
 
 
 def main():
+    """
+    Demonstration of ft_filter function.
+    """
     ages = [5, 12, 17, 18, 24, 32]
 
     def myFunc(x):
