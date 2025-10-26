3e06865c4c49b0e0a03e8063d54fe485f49161a4
MosesAwad <mawad@student.42abudhabi.ae>
2025-10-26 18:46:25 +0400

    Made some cute adjustments

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,12 +1,15 @@
 import time
 
-epoch = time.gmtime(0)
-epoch_str = time.strftime("%B %-d, %Y", epoch)
+# Date of the epoch
+epoch_date = time.gmtime(0)
+epoch_date_str = time.strftime("%B %-d, %Y", epoch_date)
 
+# Seconds since the epoch
 seconds_since_epoch = time.time()
 
-localdate = time.localtime(time.time())
+# Local date
+localdate = time.localtime(seconds_since_epoch)
 localdate_str = time.strftime("%b %d %Y",localdate)
 
-print(f"Seconds since {epoch_str}:", f"{seconds_since_epoch:,.4f}", "or", f"{seconds_since_epoch:.2e}", "in scientific notation")
+print(f"Seconds since {epoch_date_str}:", f"{seconds_since_epoch:,.4f}", "or", f"{seconds_since_epoch:.2e}", "in scientific notation")
 print(localdate_str)
