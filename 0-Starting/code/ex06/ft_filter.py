c243a9d8d42ad6e3737b0ede9993e1b729970570
Moosa Awad <mawad@student.42abudhabi.ae>
2025-10-27 22:22:17 +0400

    Removed an issue with ft_fitler

--- 0-Starting/code/ex06/ft_filter.py
+++ 0-Starting/code/ex06/ft_filter.py
@@ -3,8 +3,7 @@ def ft_filter(function, iterable):
     """filter(function or None, iterable) --> filter object
 
 Return an iterator yielding those items of iterable for which function(item)
-is true. If function is None, return the items that are true.
-    """
+is true. If function is None, return the items that are true."""
     if function is None:
         return [i for i in iterable if i]
     return [i for i in iterable if function(i) is True]
